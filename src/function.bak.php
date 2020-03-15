66282b4fa8521d1104e53b7a6abe0eec877f7158
root <root@localhost.localdomain>
2020-03-15 10:40:59 -0400

    结构整理完整

--- src/function.bak.php
+++ src/function.bak.php
@@ -1,49 +1,7 @@
 <?php
-/**
- * Link         :   http://www.phpcorner.net
- * User         :   qingbing<780042175@qq.com>
- * Extension    :   Swoole
- * Version      :   4.4.14
- * Date         :   2020-03-15
- */
-/**
- * 获取 swoole 扩展的版本号
- * @return string
- */
-function swoole_version()
-{
-    return '';
-}
-
-/**
- * 获取本机 CPU 核数
- * @return int
- */
-function swoole_cpu_num()
-{
-    return 1;
-}
-
-/**
- * 获取最近一次 Swoole 底层的错误码
- * @return int
- */
-function swoole_last_error()
-{
-    return 1;
-}
 
 // todo function
 function swoole_async_dns_lookup_coro($domain_name, $timeout = null)
-{
-}
-
-/**
- * 修改 async I/O 选项设置
- *
- * @param array $settings
- */
-function swoole_async_set($settings)
 {
 }
 
@@ -67,46 +25,9 @@ function swoole_select($read_array, $write_array, $error_array, $timeout = null)
 {
 }
 
-/**
- * 设置 process 别名
- * @param string $process_name
- */
-function swoole_set_process_name($process_name)
-{
-}
-
-/**
- * 此函数用于获取本机所有网络接口的 IP 地址
- * @return array
- */
-function swoole_get_local_ip()
-{
-    return [];
-}
-
 // todo function
 function swoole_get_local_mac()
 {
-}
-
-/**
- * 将错误代码转换成错误消息
- * @param int $errno
- * @param mixed $error_type
- * @return string
- */
-function swoole_strerror($errno, $error_type = null)
-{
-    return '';
-}
-
-/**
- * 获取最近一次系统调用的错误码
- * @return int
- */
-function swoole_errno()
-{
-    return 1;
 }
 
 // todo function
